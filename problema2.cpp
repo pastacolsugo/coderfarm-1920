a9a8aa34f80fa9a7440f67732ad9e962e972a446
Ugo Baroncini <ugobaroncini@gmail.com>
2019-10-21 16:34:37 +0200

    added problema2.cpp

---
+++ problema2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int ruote;
+	cout << "inserisci il numero di ruote: ";
+	cin >> ruote;
+	if (ruote == 2) {
+		cout << "Il veicolo e' una moto";
+	} else {
+		if (ruote == 4) {
+			cout << "Il veicolo e' un'auto";
+		} else {
+			cout << "Il veicolo e' un camion";
+		}
+	}
+}
