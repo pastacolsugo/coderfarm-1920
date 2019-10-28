ab75f2648e3d7c936e22024859c8a6d1840b78e4
Ugo Baroncini <ugobaroncini@gmail.com>
2019-10-28 15:06:07 +0100

    updated inlettere.cpp

--- Lezione 2/inlettere.cpp
+++ Lezione 2/inlettere.cpp
@@ -8,8 +8,29 @@ int main() {
 	cin >> numero;
 
 	if (numero >= 0 and numero <= 9) {
-		cout << "ok" << endl;
+		if (numero == 0) {
+			cout << "zero";
+		} else if (numero == 1) {
+			cout << "uno";
+		} else if (numero == 2) {
+			cout << "due";
+		} else if (numero == 3) {
+			cout << "tre";
+		} else if (numero == 4) {
+			cout << "quattro";
+		} else if (numero == 5) {
+			cout << "cinque";
+		} else if (numero == 6) {
+			cout << "sei";
+		} else if (numero == 7) {
+			cout << "sette";
+		} else if (numero == 8) {
+			cout << "otto";
+		} else {
+			cout << "nove";
+		}	
+		cout << endl;
 	} else {
-		cout << "not ok" << endl;
+		cout << "Il numero non e' corretto." << endl;
 	}
 }
