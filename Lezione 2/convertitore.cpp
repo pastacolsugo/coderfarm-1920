8080a57951020843bca1428c41a60dcddb040416
Ugo Baroncini <ugobaroncini@gmail.com>
2019-10-28 14:40:36 +0100

    added convertitore.cpp

---
+++ Lezione 2/convertitore.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float euro;
+	cout << "inserisci la quantita' di euro: ";
+	cin >> euro;
+	cout << "Hai " << euro * 1.11 << " dollari" << endl;
+}
