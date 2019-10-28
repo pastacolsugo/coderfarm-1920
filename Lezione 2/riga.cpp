855c2c06b716ce9770ac09c3c216a42418c8a627
Ugo Baroncini <ugobaroncini@gmail.com>
2019-10-28 16:14:28 +0100

    added riga.cpp

---
+++ Lezione 2/riga.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Inserisci n: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i = i + 1) {
+		cout << 0;
+	}
+	cout << endl;
+}
+
+
