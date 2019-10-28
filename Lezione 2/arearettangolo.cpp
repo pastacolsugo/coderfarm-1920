df8e463bf062b99b325d7cd0a461952334ccac3b
Ugo Baroncini <ugobaroncini@gmail.com>
2019-10-28 14:51:09 +0100

    added arearettangolo.cpp

---
+++ Lezione 2/arearettangolo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float latoA, latoB;
+	cout << "Inserisci il primo lato: ";
+	cin >> latoA;
+	cout << "Inserisci il secondo lato: ";
+	cin >> latoB;
+
+	cout << "L'area del rettangolo e': ";
+	cout << latoA * latoB << endl;
+}
