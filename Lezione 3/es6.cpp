c5d6274f18d87f1419f6eebc4a22c6eb2b3d1e38
Ugo Baroncini <ugobaroncini@gmail.com>
2019-11-04 15:16:59 +0100

    added 6 7

---
+++ Lezione 3/es6.cpp
@@ -0,0 +1,5 @@
+/*
+Scrivere un programma che legga un numero
+e ci dica se e' pari o dispari.
+CON MODULO.
+*/
---
+++ Lezione 3/es7.cpp
@@ -0,0 +1,5 @@
+/*
+Leggere un valore da tastiera,
+aggiungere 42 alla variabile e
+stampare il nuovo valore.
+*/
