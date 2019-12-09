15fb09fd67bddf78599a9645330ed7c86835f219
Ugo <ugobaroncini@gmail.com>
2019-12-09 15:36:12 +0100

    added es3

---
+++ Lezione 8/soluzioni-esercizi/03/esercizio3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, k, somma = 0, maxSomma = 0;
+	cin >> n >> k;
+	int qualita[n];
+	for (int i = 0; i < n; i++) {
+		cin >> qualita[i];
+	}
+	for (int i = 0; i < n - k; i++) {
+		for (int j = i; j < i + k; j++) {
+			somma += qualita[j];
+		}
+	}
+}
+
+
+
