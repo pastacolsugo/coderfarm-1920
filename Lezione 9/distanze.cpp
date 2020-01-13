a458030a1bf0e3c2c8548c42a00ffef3e4ae86a5
Ugo <ugobaroncini@gmail.com>
2020-01-13 15:04:05 +0100

    soluzione distanze.md

---
+++ Lezione 9/distanze.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <math.h>
+
+using namespace std;
+
+int main () {
+	vector <int> coordX, coordY;
+	vector <double> distanze;
+
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		coordX.push_back(x);
+		coordY.push_back(y);
+	}	
+
+	for (int i = 0; i < punti.size(); i++) {
+		double distanza = sqrt(coordX[i] * coordX[i] + coordY[i] * coordY[i]);
+
+		distanze.push_back(distanza);
+	}
+	
+	for (int i = 0; i < distanze.size(); i++) {
+		cout << distanze[i] << endl;
+	}
+}
