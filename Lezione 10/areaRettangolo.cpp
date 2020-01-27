ab47973c007c6c937a909227da8557eb02fa0a8f
Ugo <ugobaroncini@gmail.com>
2020-01-27 14:42:21 +0100

    added areaRettangolo.cpp

--- Lezione 10/areaRettangolo.cpp
+++ Lezione 10/areaRettangolo.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int areaRett(int b, int h) {
+	return b * h;
+}
+
 int main() {
 	int base = 10, altezza = 5;
 	cout << "area = ";
 	cout << areaRett(base, altezza) << endl;
-}
-
-int areaRett(int b, int h) {
-	return b * h;
 }
